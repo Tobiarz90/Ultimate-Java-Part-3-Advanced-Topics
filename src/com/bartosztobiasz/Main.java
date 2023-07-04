c9641458afab7171b924f26d5bf6f082b72f9a58
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 17:19:00 +0200

    3.11.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,8 +2,10 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        // instance of the User class or any of its derivatives (pol. pochodne)
-        Utils.printUser(new User(10));
-        Utils.printUser(new Instructor(10));
+        var users = new GenericList<User>();
+        users.add(new User(10));
+        users.add(new User(20));
+        users.add(new User(30));
+        Utils.printUsers(users);
     }
 }
--- src/com/bartosztobiasz/Utils.java
+++ src/com/bartosztobiasz/Utils.java
@@ -1,7 +1,6 @@
 package com.bartosztobiasz;
 
 public class Utils {
-    public static void printUser(User user) {
-        System.out.println(user);
+    public static void printUsers(GenericList<User> users) {
     }
 }
