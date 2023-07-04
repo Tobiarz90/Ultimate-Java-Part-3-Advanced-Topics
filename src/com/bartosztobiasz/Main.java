6417767e14b527ca06389217ae40d871228ac100
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 14:29:12 +0200

    3.4.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,15 +2,6 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new ObjectList();
-        list.add(1); // wrapper class for primitive type 'int': Integer.valueOf(1)
-        list.add("1");
-        list.add(new User());
-
-        // PROBLEMS
-        int number = (int) list.get(0); // explicit casting
-
-        // risk of invalid casting -> java.lang.ClassCastException -> RUNTIME exception
-        int number_ = (int) list.get(1);
+        new GenericList<Integer>().add("a"); // COMPILE time error -> benefit of generics
     }
 }
