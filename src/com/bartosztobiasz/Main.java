f325ddebc77138b908502ee82fbb5c7e76899d4e
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 14:07:52 +0200

    3.3.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,12 +2,15 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new List();
-        list.add(1);
-        System.out.println(list.get(0));
+        var list = new ObjectList();
+        list.add(1); // wrapper class for primitive type 'int': Integer.valueOf(1)
+        list.add("1");
+        list.add(new User());
 
-        var userList = new UserList();
-        userList.add(new User());
-        System.out.println(userList.get(0));
+        // PROBLEMS
+        int number = (int) list.get(0); // explicit casting
+
+        // risk of invalid casting -> java.lang.ClassCastException -> RUNTIME exception
+        int number_ = (int) list.get(1);
     }
 }
---
+++ src/com/bartosztobiasz/ObjectList.java
@@ -0,0 +1,14 @@
+package com.bartosztobiasz;
+
+public class ObjectList {
+    private final Object[] items = new Object[10];
+    private int count;
+
+    public void add(Object item) {
+        items[count++] = item;
+    }
+
+    public Object get(int index) {
+        return items[index];
+    }
+}
