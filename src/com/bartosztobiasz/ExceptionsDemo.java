ee09f634afc4fd8bf59b9b2bbe82e777aca555d7
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 11:31:12 +0200

    2.8.1

--- src/com/bartosztobiasz/ExceptionsDemo.java
+++ src/com/bartosztobiasz/ExceptionsDemo.java
@@ -1,25 +1,18 @@
 package com.bartosztobiasz;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("file.txt")
+        ) { // try with resources statement
             var value = reader.read();
         } catch (IOException e) {
             System.out.println("Could not read data.");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
