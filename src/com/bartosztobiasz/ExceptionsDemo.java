f94198f314d239a220bba41c4b05ef9768e1b184
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 10:45:02 +0200

    2.5.1

--- src/com/bartosztobiasz/ExceptionsDemo.java
+++ src/com/bartosztobiasz/ExceptionsDemo.java
@@ -1,11 +1,22 @@
 package com.bartosztobiasz;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
-    }
+        try {
+            var reader = new FileReader("file.txt");
 
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
+            /*
+            Code after the offending line won't be executed.
+            offending — pol. przeszkadzający, sprawiający kłopoty
+             */
+            System.out.println("File opened.");
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist.");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
