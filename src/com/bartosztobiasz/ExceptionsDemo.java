854f0c7c6e3e49c4d90fbe4d841f68070dfaa0b2
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 11:10:55 +0200

    2.6.2

--- src/com/bartosztobiasz/ExceptionsDemo.java
+++ src/com/bartosztobiasz/ExceptionsDemo.java
@@ -1,18 +1,17 @@
 package com.bartosztobiasz;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
             var value = reader.read();
-        } catch (FileNotFoundException e) { // child
-            // FileNotFoundException extends java.io.IOException
-            e.printStackTrace();
-        } catch (IOException e) { // parent
+            new SimpleDateFormat().parse("");
+        } catch (IOException | ParseException e) {
             System.out.println("Could not read data.");
         }
     }
