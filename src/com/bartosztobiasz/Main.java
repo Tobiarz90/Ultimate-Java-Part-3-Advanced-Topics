8ea8c8e43290333a1e780266b8d9321e2ae41aec
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 16:12:48 +0200

    3.8.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,5 +2,14 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
+        var user1 = new User(20);
+        var user2 = new User(10);
+
+        if (user1.compareTo(user2) < 0)
+            System.out.println("user1 < user2");
+        else if (user1.compareTo(user2) > 0)
+            System.out.println("user1 > user2");
+        else
+            System.out.println("user1 == user2");
     }
 }
--- src/com/bartosztobiasz/User.java
+++ src/com/bartosztobiasz/User.java
@@ -1,4 +1,23 @@
 package com.bartosztobiasz;
 
-public class User {
+public class User implements Comparable<User> {
+    private final int points;
+
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // this < other -> -1
+        if (points < other.points)
+            return -1;
+
+        // this == other -> 0
+        if (points == other.points)
+            return 0;
+
+        // this > other -> 1
+        return 1;
+    }
 }
