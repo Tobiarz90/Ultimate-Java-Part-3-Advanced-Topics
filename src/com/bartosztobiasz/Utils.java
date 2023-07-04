8d0e9319f73467467c5cb0520eadbda1447d4a8d
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 16:22:12 +0200

    3.9.1

---
+++ src/com/bartosztobiasz/Utils.java
@@ -0,0 +1,9 @@
+package com.bartosztobiasz;
+
+// non-generic class
+public class Utils {
+    // non-generic method
+    public static int max(int first, int second) {
+        return (first > second) ? first : second;
+    }
+}
