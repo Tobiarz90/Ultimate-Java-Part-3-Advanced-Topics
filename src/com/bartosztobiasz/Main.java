7000eb8c3f3e1386cba5025bca838446e165e1a1
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-04 17:32:17 +0200

    3.11.3

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,10 +2,15 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        var users = new GenericList<User>();
-        users.add(new User(10));
-        users.add(new User(20));
-        users.add(new User(30));
-        Utils.printUsers(users);
+        var instructors = new GenericList<Instructor>();
+        instructors.add(new Instructor(10));
+        instructors.add(new Instructor(20));
+        instructors.add(new Instructor(30));
+        /*
+         GenericList<Instructor> -> converting to bytecode -> GenericList<Object>
+         GenericList<User> -> converting to bytecode -> GenericList<Object>
+         GenericList<Object> is not a subtype of itself.
+        */
+        Utils.printUsers(instructors); // (GenericList<User>)
     }
 }
