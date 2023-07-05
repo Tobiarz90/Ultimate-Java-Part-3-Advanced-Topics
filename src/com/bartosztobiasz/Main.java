518f5ad8bb8fd78b2ab9a532619cbe49302bd5aa
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-05 15:02:35 +0200

    3.12.3

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -6,16 +6,13 @@ public class Main {
         instructors.add(new Instructor(10, "name1"));
         instructors.add(new Instructor(20, "name2"));
         instructors.add(new Instructor(30, "name3"));
-        Utils.printUsers(instructors); // (GenericList<?>)
-
-        GenericList<Integer> integers = new GenericList<>();
-        integers.add(1);
-        integers.add(2);
-        integers.add(3);
         /*
-        How to forbid passing any GenericList's type?
-        bounded wildcard (either an upper or a lower inheritance constraint)
+        (GenericList<? extends User>):
+            (GenericList<User>)
+            or
+            (GenericList<derivative of User>):
+                (GenericList<Instructor>)
          */
-        Utils.printUsers(integers);
+        Utils.printUsers(instructors);
     }
 }
--- src/com/bartosztobiasz/Utils.java
+++ src/com/bartosztobiasz/Utils.java
@@ -1,14 +1,24 @@
 package com.bartosztobiasz;
 
 public class Utils {
-    // class CAP#1 {}
-    public static void printUsers(GenericList<?> users) {
-        // cannot instantiate capture class -> cannot add anything to the list -> read-only list
+    // bounded wildcard (either an upper or a lower inheritance constraint)
+    // class CAP#1 extends User {}
+    public static void printUsers(GenericList<? extends User> users) {
+        // cannot instantiate capture class nor its subtypes -> cannot add anything to the list -> read-only list
         // users.add(new CAP#1());
 
         for (int i = 0; i < 10; i++) {
-            // unknown object from `get()` method -> assign it to `Object` type variable
-            Object object = users.get(i);
+            // either Object, User or Instructor object from `get()` method
+
+//            Object object = users.get(i);
+
+            User object = users.get(i);
+
+            // We don't know whether object from `get()` will be a `User` object or `Instructor` object.
+            // incompatible types compile time error for: Instructor object = users.get(i);
+
+//            Instructor object = (Instructor) users.get(i);
+
             System.out.println(object);
         }
     }
