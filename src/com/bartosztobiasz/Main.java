d6636266e02f6e8ead43b11c0f479b375f25b9ac
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-05 17:26:20 +0200

    4.5.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,5 +1,7 @@
 package com.bartosztobiasz;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         var list = new GenericList<String>();
@@ -7,8 +9,25 @@ public class Main {
         list.add("b");
         list.add("c");
 
-        // `list.items` -> the internal implementation of GenericList class is exposed to the outside
-        for (var item : list.items) {
+        Iterator<String> iterator = list.iterator();
+        // `Iterable` object: [a, b, c]
+        //                     ^ (pointer)
+        // `Iterator` is like a pointer.
+
+        while (iterator.hasNext()) { // Do you have more items?
+            // Yes, I do.
+
+            /*
+            `next()` method returns the element to which it points
+            and moves the pointer to the next item.
+             */
+            var current = iterator.next();
+            System.out.println(current);
+        }
+        // No, I don't.
+
+        // for each loop is a syntactical sugar over the `Iterator` object.
+        for (String item : list) {
             System.out.println(item);
         }
     }
