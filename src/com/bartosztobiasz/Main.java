ac626a74d29443726adff5050c7cbc5e78ea65ae
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-06 10:01:27 +0200

    4.10.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,17 +1,7 @@
 package com.bartosztobiasz;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("b", "e3"));
-        customers.add(new Customer("a", "e2"));
-        customers.add(new Customer("c", "e1"));
-
-        Collections.sort(customers, new EmailComparator());
-        System.out.println(customers);
+        QueueDemo.show();
     }
 }
