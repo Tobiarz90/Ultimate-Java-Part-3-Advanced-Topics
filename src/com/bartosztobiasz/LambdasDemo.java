8c07a21fb2d5da388c2be9bd5df6c6c34bc6c0eb
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-06 17:07:36 +0200

    5.4.5

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -2,9 +2,9 @@ package com.bartosztobiasz;
 
 public class LambdasDemo {
     public static void show() {
-        // lambda signature: methodName(param -> function body)
-        // one line of a function body -> remove brackets (curly braces/brackets)
-        greet(message -> System.out.println(message)); // ang. concise - pol. zwięzły, krótki, treściwy
+        // passing a Lambda Expression as a variable
+        Printer printer = message -> System.out.println(message);
+        printer.print("Hello World");
     }
 
     public static void greet(Printer printer) {
