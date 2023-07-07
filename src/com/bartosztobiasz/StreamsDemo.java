8fd416248d424d0222d21e46cfe943cb3cd545d4
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 16:42:21 +0200

    6.8.1

--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -1,6 +1,5 @@
 package com.bartosztobiasz;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class StreamsDemo {
@@ -8,29 +7,13 @@ public class StreamsDemo {
         List<Movie> movies = List.of(
                 new Movie("b", 30),
                 new Movie("a", 10),
+                new Movie("a", 10),
                 new Movie("c", 20)
         );
 
-        // sorting stream elements
-
-        // by implementing `Comparable` interface
         movies.stream()
-                .sorted() // Movie implements Comparable<Movie>
-                .forEach(System.out::println);
-
-        // by implementing `Comparator` interface as a lambda expression
-        movies.stream()
-                .sorted((a, b) -> a.getTitle().compareTo(b.getTitle()))
-                .forEach(System.out::println);
-
-        /*
-        by implementing the functional interface `Function`
-        and using this implementation
-        as a parameter of the static `comparing()` method of the `Comparator` interface
-         */
-        movies.stream()
-                .sorted(Comparator.comparing(Movie::getTitle)
-                        .reversed()) // changing order of sorting
+                .map(Movie::getLikes)
+                .distinct() // get unique items
                 .forEach(System.out::println);
     }
 }
