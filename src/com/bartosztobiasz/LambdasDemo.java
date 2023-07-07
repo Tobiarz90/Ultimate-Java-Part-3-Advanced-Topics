ffb1933eee6920832e3a2aad3fc8bfc85e6b1a1b
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 08:49:40 +0200

    5.5.1

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -1,10 +1,18 @@
 package com.bartosztobiasz;
 
 public class LambdasDemo {
-    public static void show() {
-        // passing a Lambda Expression as a variable
-        Printer printer = message -> System.out.println(message);
-        printer.print("Hello World");
+    private final String prefixField = "-";
+
+    public void show() {
+        // accessing local variables in lambda's body
+        String prefixVar = "-";
+        greet(message -> System.out.println(prefixVar + message));
+
+        // accessing (static and instance) fields in lambda's body
+        greet(message -> System.out.println(prefixField + message));
+
+        // this -> references to enclosing object -> new LambdasDemo()
+        greet(message -> System.out.println(this));
     }
 
     public static void greet(Printer printer) {
