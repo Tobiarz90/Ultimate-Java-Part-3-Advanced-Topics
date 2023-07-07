09a33bf72d757a0f7e818c7dbe72dbc7398d00a1
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 09:06:25 +0200

    5.6.1

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -1,18 +1,21 @@
 package com.bartosztobiasz;
 
 public class LambdasDemo {
-    private final String prefixField = "-";
-
-    public void show() {
-        // accessing local variables in lambda's body
-        String prefixVar = "-";
-        greet(message -> System.out.println(prefixVar + message));
-
-        // accessing (static and instance) fields in lambda's body
-        greet(message -> System.out.println(prefixField + message));
-
-        // this -> references to enclosing object -> new LambdasDemo()
-        greet(message -> System.out.println(this));
+    public static void show() {
+        // Method Reference signature: ClassName/ObjectName::methodName
+        greet(System.out::println);
+        /*
+         Tworząc Method Reference (pol. referencja do metody) [tu: System.out::println],
+         tworzymy implementację Functional Interface (pol. interfejs funkcyjny) [tu: Printer]
+         w postaci anonimowej klasy,
+         która implementuje (interfejs funkcyjny może zawierać tylko jedną metodę abstrakcyjną) metodę abstrakcyjną [tu: print()],
+         która korzysta z Method Reference [tu: println()].
+        */
+        /*
+         ClassName/ObjectName::methodName
+         - ClassName/ObjectName -> Jaka klasa lub jaki obiekt będzie implementować interfejs funkcyjny?
+         - methodName -> Jaka metoda tej klasy lub tego obiektu będzie implementować metodę abstrakcyjną tego interfejsu?
+        */
     }
 
     public static void greet(Printer printer) {
--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,6 +2,6 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        new LambdasDemo().show();
+        LambdasDemo.show();
     }
 }
