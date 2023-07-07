e91f675873b1154473cfcbfb7a1dfba0c9fba8e5
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 16:36:11 +0200

    6.7.1

--- src/com/bartosztobiasz/Movie.java
+++ src/com/bartosztobiasz/Movie.java
@@ -1,6 +1,6 @@
 package com.bartosztobiasz;
 
-public class Movie {
+public class Movie implements Comparable<Movie> {
     private final String title;
     private final int likes;
 
@@ -15,5 +15,18 @@ public class Movie {
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int compareTo(Movie other) {
+        return title.compareTo(other.getTitle());
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", likes=" + likes +
+                '}';
     }
 }
--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -1,25 +1,36 @@
 package com.bartosztobiasz;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamsDemo {
     public static void show() {
         List<Movie> movies = List.of(
+                new Movie("b", 30),
                 new Movie("a", 10),
-                new Movie("c", 30),
-                new Movie("b", 15)
+                new Movie("c", 20)
         );
 
+        // sorting stream elements
+
+        // by implementing `Comparable` interface
         movies.stream()
-                .takeWhile(movie -> movie.getLikes() < 30) // takes elements matching a criterion
-                .map(Movie::getTitle)
+                .sorted() // Movie implements Comparable<Movie>
                 .forEach(System.out::println);
 
-        System.out.println("---");
-
+        // by implementing `Comparator` interface as a lambda expression
         movies.stream()
-                .dropWhile(movie -> movie.getLikes() < 30) // skips elements matching a criterion
-                .map(Movie::getTitle)
+                .sorted((a, b) -> a.getTitle().compareTo(b.getTitle()))
+                .forEach(System.out::println);
+
+        /*
+        by implementing the functional interface `Function`
+        and using this implementation
+        as a parameter of the static `comparing()` method of the `Comparator` interface
+         */
+        movies.stream()
+                .sorted(Comparator.comparing(Movie::getTitle)
+                        .reversed()) // changing order of sorting
                 .forEach(System.out::println);
     }
 }
