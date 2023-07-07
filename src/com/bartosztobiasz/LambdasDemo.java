72490573993e83768d228b0e39758b1438974859
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 09:29:35 +0200

    5.6.4

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -1,15 +1,14 @@
 package com.bartosztobiasz;
 
 public class LambdasDemo {
-    public static void show() {
-        // passing instance methods as a Method Reference
-        var demo = new LambdasDemo();
-        greet(message -> demo.printInstance(message));
-        greet(demo::printInstance); // ObjectName::methodName
+    public LambdasDemo(String message) {
+        System.out.println(message);
     }
 
-    public void printInstance(String message) {
-        System.out.println(message);
+    public static void show() {
+        // passing value to a class constructor as a Method Reference
+        greet(message -> new LambdasDemo(message));
+        greet(LambdasDemo::new);
     }
 
     public static void greet(Printer printer) {
