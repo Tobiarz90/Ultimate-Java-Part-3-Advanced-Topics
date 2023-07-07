588874e351b02f82039c2e94db6992a12dbcbdd2
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 16:16:23 +0200

    6.6.3

--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -6,16 +6,19 @@ public class StreamsDemo {
     public static void show() {
         List<Movie> movies = List.of(
                 new Movie("a", 10),
-                new Movie("b", 15),
-                new Movie("c", 20)
+                new Movie("c", 30),
+                new Movie("b", 15)
         );
 
-        // data pagination (pol. dzielenie na strony)
-        int page = 3;
-        int pageSize = 10;
         movies.stream()
-                .skip((page - 1) * pageSize)
-                .limit(pageSize)
+                .takeWhile(movie -> movie.getLikes() < 30) // takes elements matching a criterion
+                .map(Movie::getTitle)
+                .forEach(System.out::println);
+
+        System.out.println("---");
+
+        movies.stream()
+                .dropWhile(movie -> movie.getLikes() < 30) // skips elements matching a criterion
                 .map(Movie::getTitle)
                 .forEach(System.out::println);
     }
