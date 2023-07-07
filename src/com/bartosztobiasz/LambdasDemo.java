716bab9991ddb366000669dcf845dcd0d8d71d25
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 10:01:46 +0200

    5.8.1

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -1,17 +1,43 @@
 package com.bartosztobiasz;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdasDemo {
-    public LambdasDemo(String message) {
-        System.out.println(message);
-    }
-
     public static void show() {
-        // passing value to a class constructor as a Method Reference
-        greet(message -> new LambdasDemo(message));
-        greet(LambdasDemo::new);
-    }
+        List<Integer> list = List.of(1, 2, 3);
 
-    public static void greet(Printer printer) {
-        printer.print("Hello World");
+        // Imperative Programming (for, if/else, switch/case) - programming style/paradigm
+        // -> implementing logic using instructions; HOW it should be done?
+        for (Integer item : list) {
+            System.out.println(item);
+        }
+
+        // Declarative Programming - programming style/paradigm
+        // -> WHAT should be done?
+
+        // #1 - anonymous inner class
+        list.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer item) {
+                System.out.println(item);
+            }
+        });
+
+        // #2 - lambda expression
+        list.forEach((Integer item) -> {
+            System.out.println(item);
+        });
+
+        // #3 - no type specified
+        list.forEach((item) -> {
+            System.out.println(item);
+        });
+
+        // #4 - single argument and one-line body
+        list.forEach(item -> System.out.println(item));
+
+        // #5 - method reference
+        list.forEach(System.out::println);
     }
 }
