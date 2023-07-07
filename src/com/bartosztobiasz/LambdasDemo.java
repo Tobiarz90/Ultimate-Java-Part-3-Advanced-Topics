b5a6a83ca4fd29e6708109fa2fa0cbc9e58a0e27
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-07 09:16:14 +0200

    5.6.2

--- src/com/bartosztobiasz/LambdasDemo.java
+++ src/com/bartosztobiasz/LambdasDemo.java
@@ -2,20 +2,13 @@ package com.bartosztobiasz;
 
 public class LambdasDemo {
     public static void show() {
-        // Method Reference signature: ClassName/ObjectName::methodName
-        greet(System.out::println);
-        /*
-         Tworząc Method Reference (pol. referencja do metody) [tu: System.out::println],
-         tworzymy implementację Functional Interface (pol. interfejs funkcyjny) [tu: Printer]
-         w postaci anonimowej klasy,
-         która implementuje (interfejs funkcyjny może zawierać tylko jedną metodę abstrakcyjną) metodę abstrakcyjną [tu: print()],
-         która korzysta z Method Reference [tu: println()].
-        */
-        /*
-         ClassName/ObjectName::methodName
-         - ClassName/ObjectName -> Jaka klasa lub jaki obiekt będzie implementować interfejs funkcyjny?
-         - methodName -> Jaka metoda tej klasy lub tego obiektu będzie implementować metodę abstrakcyjną tego interfejsu?
-        */
+        // passing static methods as a Method Reference
+        greet(message -> printStatic(message));
+        greet(LambdasDemo::printStatic); // ClassName::methodName
+    }
+
+    public static void printStatic(String message) {
+        System.out.println(message);
     }
 
     public static void greet(Printer printer) {
