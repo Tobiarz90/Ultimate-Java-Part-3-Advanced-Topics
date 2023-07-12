5e770f3c180064bfb0ba7649390a78057bf727f5
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-12 09:30:15 +0200

    6.10.3

--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -10,8 +10,8 @@ public class StreamsDemo {
                 new Movie("c", 30)
         );
 
-        boolean anyMatch = movies.stream()
-                .anyMatch(movie -> movie.getLikes() > 20);
-        System.out.println(anyMatch);
+        boolean allMatch = movies.stream()
+                .allMatch(movie -> movie.getLikes() > 10);
+        System.out.println(allMatch);
     }
 }
