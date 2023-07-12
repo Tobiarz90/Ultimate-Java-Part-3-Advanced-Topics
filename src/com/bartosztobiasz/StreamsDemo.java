62fa52c38395b8327aa659e072b9e0b7b917c4b5
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-12 09:51:05 +0200

    6.11.1

--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -1,6 +1,5 @@
 package com.bartosztobiasz;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class StreamsDemo {
@@ -11,9 +10,25 @@ public class StreamsDemo {
                 new Movie("c", 30)
         );
 
-        Movie popularMovie = movies.stream()
-                .max(Comparator.comparing(Movie::getLikes))
-                .get();
-        System.out.println(popularMovie);
+        // get total likes from all movies
+
+        /*
+        .reduce(BinaryOperator) method explanation:
+        [10, 20, 30]
+        [a=10, b=20, 30]
+        [a + b, 30]
+        [30, 30]
+        [a=30, b=30]
+        [a + b]
+        [60]
+        60
+         */
+        int sumOfLikes = movies.stream()
+                .map(Movie::getLikes)
+//                .reduce((a, b) -> a + b)
+//                .reduce((a, b) -> Integer.sum(a, b))
+                .reduce(Integer::sum)
+                .orElse(0);
+        System.out.println(sumOfLikes);
     }
 }
