f510b580f2643f121100151319f7f7899fe864a6
Bartosz Tobiasz <barticz90@gmail.com>
2023-07-12 09:55:21 +0200

    6.11.2

--- src/com/bartosztobiasz/StreamsDemo.java
+++ src/com/bartosztobiasz/StreamsDemo.java
@@ -12,23 +12,9 @@ public class StreamsDemo {
 
         // get total likes from all movies
 
-        /*
-        .reduce(BinaryOperator) method explanation:
-        [10, 20, 30]
-        [a=10, b=20, 30]
-        [a + b, 30]
-        [30, 30]
-        [a=30, b=30]
-        [a + b]
-        [60]
-        60
-         */
         int sumOfLikes = movies.stream()
                 .map(Movie::getLikes)
-//                .reduce((a, b) -> a + b)
-//                .reduce((a, b) -> Integer.sum(a, b))
-                .reduce(Integer::sum)
-                .orElse(0);
+                .reduce(0, Integer::sum); // .reduce(initialValue, BinaryOperator)
         System.out.println(sumOfLikes);
     }
 }
